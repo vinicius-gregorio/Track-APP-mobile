52caa8f83854ea1fde2ddbabdfb6611356578504
Vinícius Gregório <vincamgreg@hotmail.com>
2020-12-29 19:20:23 -0300

    Navigation Button is now from react-native-elements library

--- src/components/NavigationButton.js
+++ src/components/NavigationButton.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const NavigationButton = ({ onPress, title }) => {
-    return <>
-    <TouchableOpacity onPress={onPress}  style={styles.appButtonContainer}>
-    <Text style={styles.appButtonText}>{title}</Text>
-    </TouchableOpacity>    
-    </>
+    // return <>
+    // <TouchableOpacity onPress={onPress}  style={styles.appButtonContainer}>
+    // <Text style={styles.appButtonText}>{title}</Text>
+    // </TouchableOpacity>    
+    // </>
+    return <Button title={title}
+     onPress = {onPress}
+      buttonStyle={styles.appButtonContainer} 
+      
+      />;
 }
 
 const styles = StyleSheet.create({
     appButtonContainer: {
         elevation: 8,
-        backgroundColor: "#00adb5",
+        backgroundColor: "#333",
         borderRadius: 10,
         paddingVertical: 10,
         paddingHorizontal: 12,
         marginHorizontal: 10,
-        marginVertical: 10,
+        
+        
       },
       appButtonText: {
         fontSize: 18,
