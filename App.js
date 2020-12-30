6c4057988fe895caa65bb0bde8944d253e035a41
Vinícius Gregório <vincamgreg@hotmail.com>
2020-12-30 20:18:38 -0300

    navigation Ref.js

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
+import { setNavigator } from './src/navigationRef';
 const switchNavigator = createSwitchNavigator({
     loginFlow: createStackNavigator({ 
         Signup: SignupScreen,
@@ -32,7 +33,7 @@ const App =  createAppContainer(switchNavigator);
 export default () => {
     return (
         <AuthProvider>
-            <App/>
+            <App ref={(navigator) => { setNavigator(navigator)}}/>
         </AuthProvider>
     );
 };
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,29 +1,35 @@
+import { AsyncStorage } from '@react-native-community/async-storage';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import { navigate} from '../navigationRef';
 
 const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error': 
             return {...state, errorMessage: action.payload};
             break;
-    
+        case 'signup': 
+            return {errorMessage: '', token: action.payload};
+            break;
         default:
             return state;
             break;
     }
 };
 
-const signup = (dispatch) => {
-    return async ({ email, password }) => {
+const signup = (dispatch) => async ({ email, password }) => {
         //TODO: make api request
         try {
             const response = await trackerApi.post('/signup', { email, password});
-            console.log(response.data);
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({ type: 'signup', payload: response.data.token});
+           // await AsyncStorage.getItem('token');
+           navigate('TrackList');
         } catch (error) {
             dispatch({ type: 'add_error', payload: 'Something went wrong with sign up'});
         }
-    }
-};
+    };
+
 
 const signin = (dispatch) => {
     return ({ email, password }) => {
@@ -38,5 +44,5 @@ const signout = (dispatch) => {
 export const { Provider, Context} = createDataContext(
     authReducer,
     { signin, signup, signout },
-    { isSignedIn: false, errorMessage: '' }
+    { token: null, errorMessage: '' }
 );
